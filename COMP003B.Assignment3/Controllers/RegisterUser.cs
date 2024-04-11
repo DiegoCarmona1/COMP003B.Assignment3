dadb51b512eacd41ce620fcf83e3604647e5d736
Diego Carmona <ultgojifan@gmail.com>
2024-04-11 01:59:11 -0700

    Linked models to controller UserController. Next, Created New Model User.cs. Lastly added public class User to Model User.cs

--- COMP003B.Assignment3/Controllers/RegisterUser.cs
+++ COMP003B.Assignment3/Controllers/RegisterUser.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using COMP003B.Assignment3.Models;
 
 namespace COMP003B.Assignment3.Controllers
 {
     public class RegisterUser : Controller
     {
-        private static List<Users> _users = new List<Users>();
+        private static List<User> _users = new List<User>();
         public IActionResult Index()
         {
             return View(_users);
@@ -16,6 +17,65 @@ namespace COMP003B.Assignment3.Controllers
             return View();
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(User users)
+        {
+            if (ModelState.IsValid)
+            {
+                if (!_users.Any(u => u.Id == users.Id))
+                {
+                    _users.Add(users);
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
 
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = _users.FirstOrDefault(u => u.Id == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, User users)
+        {
+            if (id != User.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                var existingUser = _users.FirstOrDefault(u => u.Id == users.Id);
+
+                if (existingUser != null)
+                {
+                    existingUser.Name = users.Name;
+                    existingUser.Email = users.Email;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(users);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+
+        }
     }
 }
---
+++ COMP003B.Assignment3/Models/UserModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.Assignment3.Models
+{
+    public class User
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Full Name")]
+        public string Name { get; set; }
+        [Required]
+        [Range(0,120)]
+        public int Age { get; set; }
+    }
+}
