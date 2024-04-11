071607c1ac78e32bcdc3cae403e272284c2014b8
Diego Carmona <ultgojifan@gmail.com>
2024-04-11 00:11:27 -0700

    Add project files.

---
+++ COMP003B.Assignment3/Controllers/RegisterUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Assignment3.Controllers
+{
+    public class RegisterUser : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
