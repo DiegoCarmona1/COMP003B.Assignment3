d4f6444fa5c492cc0e7dfc1c74b3ce6c56991b26
Diego Carmona <ultgojifan@gmail.com>
2024-04-11 00:15:24 -0700

    Added List of users, index now shows list of users view, added create action.

--- COMP003B.Assignment3/Controllers/RegisterUser.cs
+++ COMP003B.Assignment3/Controllers/RegisterUser.cs
@@ -4,7 +4,14 @@ namespace COMP003B.Assignment3.Controllers
 {
     public class RegisterUser : Controller
     {
+        private static List<Users> _users = new List<Users>();
         public IActionResult Index()
+        {
+            return View(_users);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
         {
             return View();
         }
